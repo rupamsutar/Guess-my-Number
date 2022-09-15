28f0603c185aa137ee50c9b84f45cf52da7664f6
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-15 14:54:01 +0530

    Handling Click Events

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "😵 No Number !";
+  }
+});
